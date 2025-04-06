fa3e553fc6d817c4d3e7298de966e090417a6440
elahe-fakour <elahe.fakoor16@gmail.com>
2025-04-06 14:48:36 +0330

    Nearly finished pet card feature.

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ async function petsArea() {
     const clone = template.content.cloneNode(true)
 
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} name ${pet.name}.`
 
     wrapper.appendChild(clone)
   })
@@ -24,3 +28,13 @@ async function petsArea() {
 }
 
 petsArea()
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
+
+  if (age == 1) return "1 years old"
+  if (age == 0) return "Less than a year old"
+
+  return `${age} years old`
+}
